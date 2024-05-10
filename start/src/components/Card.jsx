d28f650a1f13201be406c37eec4506ac0d45c002
jochris15 <jochris15@gmail.com>
2024-05-10 11:06:01 +0700

    change start a bit

--- start/src/components/Card.jsx
+++ start/src/components/Card.jsx
@@ -9,6 +9,7 @@ export default function Card({ product }) {
                 />
                 <button className="btn btn-accent btn-sm m-1 w-full">Detail</button>
                 <button className="btn btn-error btn-sm m-1 w-full">Delete</button>
+                <button className="btn btn-warning btn-sm m-1 w-full">Edit</button>
             </figure>
             <div className="card-body flex-1">
                 <b>{product.name}</b>
